12e03afb6a802477ee63419cbb50a0605a66b33a
dangthevang <dangthevang2000@gmail.com>
2022-06-08 14:22:38 +0700

    CapNhatDanhSachTong

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import pandas as pd
 # web.get_Income("AAA",year=2022,month=1,type_="Y",times=10).to_csv("AAA.csv")
 import multiprocessing
 
-def run1(symbol):
+def KeoBaoCaoTaiChinh(symbol):
   time = 20
   web = F.FinancailStatement()
   df = pd.DataFrame({})
@@ -21,33 +21,37 @@ def run1(symbol):
   df.to_csv("Data/Financial/"+symbol+".csv",index=False)
   print(symbol,df.columns)
   # web.driver.close()
-def run2(symbol,link):
+def KeoCoTuc(symbol,link):
   web = F.Dividend()
   df = pd.DataFrame({})
   # t = web.getTable('hose/FCN-cong-ty-co-phan-fecon.chn')
   df = web.FilterData(link)
   df.to_csv("Data/Dividend/"+symbol+".csv",index=False)
   # web.driver.close()
-def run3(symbol,link):
+def LayVoLumeHienTai(symbol,link):
   web = F.Volume()
   df = pd.DataFrame({})
   # t = web.getTable('hose/FCN-cong-ty-co-phan-fecon.chn')
   df = web.getVolumeNow(link)
   df.to_csv("Data/Volume/"+symbol+".csv",index=False)
   # web.driver.close()
-def multip():
-    data = pd.read_csv("all.csv")
-    data2 = pd.read_excel("List_Com_First (1_4).xlsx",engine="openpyxl")
-    data = data2.merge(data,on="Symbol",how="left")
-    Symbol = data["Symbol"][0:500]
-    Link = data["Link"][0:500]
-    # Symbol = ["A32"]
-    pool = multiprocessing.Pool(processes=2)
-    for symbol in range(len(Symbol)):
-      pool.apply_async(run1,args=(Symbol[symbol],))
-    pool.close()
-    pool.join()
+def LayDanhSachCongTy():
+  web = F.ListCompany()
+  web.get_all_symbol().to_csv("TatCaCongTy.csv")
+LayDanhSachCongTy()
+# def multip():
+#     data = pd.read_csv("all.csv")
+#     data2 = pd.read_excel("List_Com_First (1_4).xlsx",engine="openpyxl")
+#     data = data2.merge(data,on="Symbol",how="left")
+#     Symbol = data["Symbol"][0:500]
+#     Link = data["Link"][0:500]
+#     # Symbol = ["A32"]
+#     pool = multiprocessing.Pool(processes=2)
+#     for symbol in range(len(Symbol)):
+#       pool.apply_async(run1,args=(Symbol[symbol],))
+#     pool.close()
+#     pool.join()
 
-if __name__ == '__main__':
-    multip()
+# if __name__ == '__main__':
+#     multip()
       
